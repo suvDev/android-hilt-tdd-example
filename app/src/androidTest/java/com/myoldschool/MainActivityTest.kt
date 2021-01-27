b5cfdd75aebe33ff6b8bf4e9fca1c1fa45b58dd0
suvdev.work <suvdev.work@gmail.com>
2021-01-27 23:38:04 +0530

    added sample test to check progress bar visibility

--- app/src/androidTest/java/com/myoldschool/MainActivityTest.kt
+++ app/src/androidTest/java/com/myoldschool/MainActivityTest.kt
@@ -1,9 +1,25 @@
 package com.myoldschool
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
+
+    @Rule
+    var mActivityRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testProgressBarVisible(){
+        onView(withId(R.id.progressbar)).check(matches(isDisplayed()))
+    }
 
 }
