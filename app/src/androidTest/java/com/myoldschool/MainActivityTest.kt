782449a5989acdf4d47f131677f7cf2f5eb5ca24
suvdev.work <suvdev.work@gmail.com>
2021-01-30 23:08:29 +0530

    ocked api in ui test

--- app/src/androidTest/java/com/myoldschool/MainActivityTest.kt
+++ app/src/androidTest/java/com/myoldschool/MainActivityTest.kt
@@ -2,13 +2,17 @@ package com.myoldschool
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.io.IOException
 
 
 @RunWith(AndroidJUnit4::class)
@@ -16,6 +20,24 @@ class MainActivityTest {
 
     @get:Rule
     var mActivityRule = ActivityScenarioRule(MainActivity::class.java)
+    private val mockWebServer: MockWebServer = MockWebServer()
+    private val MOCK_WEBSERVER_PORT = 5000
+
+
+    @Before
+    @Throws(IOException::class, InterruptedException::class)
+    fun setup() {
+        mockWebServer.apply {
+            enqueue(MockResponse().setBody(MockRespFileReader("student.json").content))
+        }
+        mockWebServer.start(MOCK_WEBSERVER_PORT)
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun teardown() {
+        mockWebServer.shutdown()
+    }
 
     @Test
     fun testProgressBarVisible(){
@@ -23,6 +45,7 @@ class MainActivityTest {
         // When: MainActivity is launched
         // Then: Progress bar should be visible
         onView(withId(R.id.progressbar)).check(matches(isDisplayed()))
+        Thread.sleep(10000)
     }
 
 }
---
+++ app/src/androidTest/java/com/myoldschool/MockRespFileReader.kt
@@ -0,0 +1,13 @@
+package com.myoldschool
+
+import java.io.InputStreamReader
+
+class MockRespFileReader(path: String) {
+    val content: String
+
+    init {
+        val reader = InputStreamReader(this.javaClass.classLoader!!.getResourceAsStream(path))
+        content = reader.readText()
+        reader.close()
+    }
+}
